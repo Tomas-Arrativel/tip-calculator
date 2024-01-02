01315cc49febc13ebb3716729724a26c00ecce72
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-02 10:05:15 -0300

    structure for the tip % selector

--- src/containers/Form/Form.tsx
+++ src/containers/Form/Form.tsx
@@ -8,6 +8,17 @@ const Form = () => {
         <img src='./images/icon-dollar.svg' alt='Dollar icon' />
         <input type='text' placeholder='0' />
       </div>
+      <div>
+        <label>Select Tip %</label>
+        <div>
+          <button>5%</button>
+          <button>10%</button>
+          <button>15%</button>
+          <button>25%</button>
+          <button>50%</button>
+          <input type='text' placeholder='Custom' />
+        </div>
+      </div>
     </form>
   );
 };
