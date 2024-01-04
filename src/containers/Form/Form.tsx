c64cd0a1bf7facaa3cfa3957f850af2048da93a9
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-04 09:21:02 -0300

    Adding the styles to the tip inputs

--- src/containers/Form/Form.tsx
+++ src/containers/Form/Form.tsx
@@ -8,16 +8,25 @@ const Form = () => {
         <img src='./images/icon-dollar.svg' alt='Dollar icon' />
         <input type='text' placeholder='0' />
       </div>
-      <div>
+
+      <div className={styles.tip}>
         <label>Select Tip %</label>
-        <div>
+        <div className={styles.tip__button}>
           <button>5%</button>
           <button>10%</button>
           <button>15%</button>
+        </div>
+        <div className={styles.tip__button}>
           <button>25%</button>
           <button>50%</button>
           <input type='text' placeholder='Custom' />
         </div>
+      </div>
+
+      <div className={styles.input}>
+        <label>Number of people</label>
+        <img src='./images/icon-person.svg' alt='Person icon' />
+        <input type='text' placeholder='0' />
       </div>
     </form>
   );
