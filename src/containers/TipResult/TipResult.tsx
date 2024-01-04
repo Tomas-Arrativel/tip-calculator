cbd16c9d031dc3a03b3aee3a875938c931cb7d9c
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-04 09:40:56 -0300

    Making the tip results and the styling

--- src/containers/TipResult/TipResult.tsx
+++ src/containers/TipResult/TipResult.tsx
@@ -1,7 +1,25 @@
 import styles from './TipResult.module.css';
 
 const TipResult = () => {
-  return <section className={styles.container}>TipResult</section>;
+  return (
+    <section className={styles.container}>
+      <div className={styles.tips}>
+        <div className={styles.textCont}>
+          <p>Tip Amount</p>
+          <span>/ person</span>
+        </div>
+        <p className={styles.tipNumbers}>$0.00</p>
+      </div>
+
+      <div className={styles.tips}>
+        <div className={styles.textCont}>
+          <p>Total</p>
+          <span>/ person</span>
+        </div>
+        <p className={styles.tipNumbers}>$0.00</p>
+      </div>
+    </section>
+  );
 };
 
 export default TipResult;
