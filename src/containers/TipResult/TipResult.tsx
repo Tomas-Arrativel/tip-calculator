e20425a3b8db5c86d27bffa13dc8076126354e77
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-04 09:43:55 -0300

    changing something in the style

--- src/containers/TipResult/TipResult.tsx
+++ src/containers/TipResult/TipResult.tsx
@@ -3,21 +3,24 @@ import styles from './TipResult.module.css';
 const TipResult = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.tips}>
-        <div className={styles.textCont}>
-          <p>Tip Amount</p>
-          <span>/ person</span>
+      <div className={styles.tipContainer}>
+        <div className={styles.tips}>
+          <div className={styles.textCont}>
+            <p>Tip Amount</p>
+            <span>/ person</span>
+          </div>
+          <p className={styles.tipNumbers}>$0.00</p>
         </div>
-        <p className={styles.tipNumbers}>$0.00</p>
-      </div>
 
-      <div className={styles.tips}>
-        <div className={styles.textCont}>
-          <p>Total</p>
-          <span>/ person</span>
+        <div className={styles.tips}>
+          <div className={styles.textCont}>
+            <p>Total</p>
+            <span>/ person</span>
+          </div>
+          <p className={styles.tipNumbers}>$0.00</p>
         </div>
-        <p className={styles.tipNumbers}>$0.00</p>
       </div>
+      <button className={styles.btn}>Reset</button>
     </section>
   );
 };
